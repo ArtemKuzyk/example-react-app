13cc111f8ec7435258a872312fec371e538c2903
Artem_Kuzyk <artem.brar@gmail.com>
2023-02-05 23:24:33 +0200

    test commit

--- src/services/bookDataLoader.js
+++ src/services/bookDataLoader.js
@@ -2,15 +2,19 @@ import axios from "axios";
 import { LS_KEYS, LocalStorageService } from "./localStorage";
 
 const DATA_URL = {PATH : "./book.JSON",
-                  GIT_PATH: "https://artemkuzyk.github.io/data-base/book.JSON"};
+                  GIT_PATH: "https://artemkuzyk.github.io/book.JSON"};
 
 class BookData{
     static async set(path){
         // return await axios
-        return fetch(DATA_URL.GIT_PATH)
+        console.log(DATA_URL.GIT_PATH)
+        return await fetch(DATA_URL.GIT_PATH)
         // .get(DATA_URL.GIT_PATH)
         // .then((response) => response.data)
-        .then((response) => response.json())
+        .then((response) => {
+            console.log(response);
+            return response.json()}
+            )
         .then((data) => Object.values(data))
         .then((bookObj) => Object.keys(bookObj[0]).map(el => bookObj[0][el]))
         .then((bookArray) => {LocalStorageService.set(LS_KEYS.BOOK_LIST, bookArray)})
