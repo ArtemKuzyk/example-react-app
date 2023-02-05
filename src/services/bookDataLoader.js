6d7370d12a06443012c21519a374e4f632bbff3e
Artem_Kuzyk <artem.brar@gmail.com>
2023-02-05 23:18:09 +0200

    test commit

--- src/services/bookDataLoader.js
+++ src/services/bookDataLoader.js
@@ -2,13 +2,13 @@ import axios from "axios";
 import { LS_KEYS, LocalStorageService } from "./localStorage";
 
 const DATA_URL = {PATH : "./book.JSON",
-                  GIT_PATH: "https://artemkuzyk.github.io/book.JSON"};
+                  GIT_PATH: "https://artemkuzyk.github.io/data-base/book.JSON"};
 
 class BookData{
     static async set(path){
         // return await axios
-        console.log(DATA_URL.GIT_PATH)
-        return fetch(DATA_URL.GIT_PATH)
+        console.log(DATA_URL.PATH)
+        return fetch(DATA_URL.PATH)
         // .get(DATA_URL.GIT_PATH)
         // .then((response) => response.data)
         .then((response) => {
